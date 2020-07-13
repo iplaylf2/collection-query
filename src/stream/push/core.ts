9b243239094e2535f85c552b0b9706dd47aad33a
iplaylf2 <914637118@qq.com>
2020-07-13 23:34:46 +0800

    change

--- src/stream/push/core.ts
+++ src/stream/push/core.ts
@@ -2,67 +2,75 @@ import { EmitForm } from "./emitter";
 import { Selector, Predicate } from "../../util";
 import { PushType } from "./push-type";
 
-export function map<T, K>(emit: EmitForm<K, never>, f: Selector<T, K>, x: T) {
-  emit(PushType.Next, f(x));
+export function map<T, K>(emit: EmitForm<K, never>, f: Selector<T, K>) {
+  return (x: T) => {
+    emit(PushType.Next, f(x));
+  };
 }
 
-export function filter<T>(emit: EmitForm<T, never>, f: Predicate<T>, x: T) {
-  if (f(x)) {
-    emit(PushType.Next, x);
-  }
+export function filter<T>(emit: EmitForm<T, never>, f: Predicate<T>) {
+  return (x: T) => {
+    if (f(x)) {
+      emit(PushType.Next, x);
+    }
+  };
 }
 
-export function remove<T>(emit: EmitForm<T, never>, f: Predicate<T>, x: T) {
-  if (!f(x)) {
-    emit(PushType.Next, x);
-  }
+export function remove<T>(emit: EmitForm<T, never>, f: Predicate<T>) {
+  return (x: T) => {
+    if (!f(x)) {
+      emit(PushType.Next, x);
+    }
+  };
 }
 
-export function take<T>(emit: EmitForm<T, never>, o: { n: number }, x: T) {
-  if (o.n > 0) {
-    o.n--;
-    emit(PushType.Next, x);
-  } else {
-    emit(PushType.Complete);
-  }
-}
-
-export function takeWhile<T>(emit: EmitForm<T, never>, f: Predicate<T>, x: T) {
-  if (f(x)) {
-    emit(PushType.Next, x);
-  } else {
-    emit(PushType.Complete);
-  }
-}
-
-export function skip<T>(
-  emit: EmitForm<T, never>,
-  o: { n: number; skip: boolean },
-  x: T
-) {
-  if (o.skip) {
-    if (o.n > 0) {
-      o.n--;
+export function take<T>(emit: EmitForm<T, never>, n: number) {
+  return (x: T) => {
+    if (n > 0) {
+      n--;
+      emit(PushType.Next, x);
     } else {
-      o.skip = false;
-      emit(PushType.Next, x);
+      emit(PushType.Complete);
     }
-  } else {
-    emit(PushType.Next, x);
-  }
+  };
 }
 
-export function skipWhile<T>(
-  emit: EmitForm<T, never>,
-  o: { f: Predicate<T>; skip: boolean },
-  x: T
-) {
-  if (o.skip) {
-    if (!o.f(x)) {
-      o.skip = false;
+export function takeWhile<T>(emit: EmitForm<T, never>, f: Predicate<T>) {
+  return (x: T) => {
+    if (f(x)) {
+      emit(PushType.Next, x);
+    } else {
+      emit(PushType.Complete);
+    }
+  };
+}
+
+export function skip<T>(emit: EmitForm<T, never>, n: number) {
+  let skip = true;
+  return (x: T) => {
+    if (skip) {
+      if (n > 0) {
+        n--;
+      } else {
+        skip = false;
+        emit(PushType.Next, x);
+      }
+    } else {
       emit(PushType.Next, x);
     }
-  } else {
-    emit(PushType.Next, x);
-  }
+  };
+}
+
+export function skipWhile<T>(emit: EmitForm<T, never>, f: Predicate<T>) {
+  let skip = true;
+  return (x: T) => {
+    if (skip) {
+      if (!f(x)) {
+        skip = false;
+        emit(PushType.Next, x);
+      }
+    } else {
+      emit(PushType.Next, x);
+    }
+  };
 }
