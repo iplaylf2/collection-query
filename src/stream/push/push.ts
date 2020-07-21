a498d1daa9aef9c8da047a71d3d7a78e699a677e
i-drink-yakult <xiaodong@basefex.com>
2020-07-21 17:48:15 +0800

    use symbol

--- src/stream/push/push.ts
+++ src/stream/push/push.ts
@@ -1,9 +1,20 @@
-import { Emitter } from "./type";
+import { Action } from "../../type";
+import { EmitForm, Emitter } from "./type";
+import { createEmitter } from "./create-emitter";
+
+const emitterSymbol = Symbol("emitter");
 
 export default class Push<T, Te = any> {
-  constructor(emitter: Emitter<T, Te>) {
-    this.emitter = emitter;
+  static create<T, Te>(executor: Action<EmitForm<T, Te>>) {
+    const emitter = createEmitter(executor);
+    return new Push(emitter);
   }
 
-  emitter: Emitter<T, Te>;
+  static emitter: typeof emitterSymbol = emitterSymbol;
+
+  constructor(emitter: Emitter<T, Te>) {
+    this[emitterSymbol] = emitter;
+  }
+
+  [emitterSymbol]: Emitter<T, Te>;
 }
