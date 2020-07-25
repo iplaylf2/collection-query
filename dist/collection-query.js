527e7a4e8b7879453a718b66a8be516822f62421
i-drink-yakult <xiaodong@basefex.com>
2020-07-25 13:17:22 +0800

    build

--- dist/collection-query.js
+++ dist/collection-query.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pipe = void 0;
+var type_1 = require("./stream/push/type");
+Object.defineProperty(exports, "EmitType", { enumerable: true, get: function () { return type_1.EmitType; } });
 function pipe(list) {
     return (s) => list.reduce((r, f) => f(r), s);
 }
--- type/collection-query.d.ts
+++ type/collection-query.d.ts
@@ -1,1 +1,4 @@
+export { PullStream, AsyncPullStream, PushStream, AsyncPushStream, } from "./stream/type";
+export { IterateItem } from "./stream/pull/type";
+export { EmitType, EmitItem } from "./stream/push/type";
 export declare function pipe<T = any, K = T>(list: any[]): (x: T) => K;
