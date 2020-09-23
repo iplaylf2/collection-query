f1bf877527d6718c4719bc956cfbd2aee1e43521
i-drink-yakult <xiaodong@basefex.com>
2020-09-23 11:54:04 +0800

    finsih LinkedList

--- src/async-tool/linked-list.ts
+++ src/async-tool/linked-list.ts
@@ -1,18 +1,37 @@
 export class LinkedList<T> {
   constructor() {
-    this.head = {} as any;
-    this.tail = this.head;
+    const tail = {} as any;
+    this.head = { next: tail } as any;
+    this.tail = tail;
+    this._length = 0;
   }
 
   put(x: T) {
-      
+    const tail = {} as any;
+    this.tail.value = x;
+    this.tail.next = tail;
+    this.tail = tail;
+    this._length++;
   }
 
-  take(): T {}
+  take(): T {
+    if (this._length > 0) {
+      const current = this.head.next!;
+      const x = current.value;
+      this.head.next = current.next;
+      this._length--;
+      return x;
+    }
+    throw "empty";
+  }
+
+  get length() {
+    return this._length;
+  }
 
   private head: LinkNode<T>;
   private tail: LinkNode<T>;
-  private length: number;
+  private _length: number;
 }
 
 interface LinkNode<T> {
