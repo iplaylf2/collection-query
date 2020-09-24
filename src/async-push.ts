74329a58746081028e2631ea0677e89a6e38ce3e
i-drink-yakult <xiaodong@basefex.com>
2020-09-24 14:19:13 +0800

    add export

--- src/async-push.ts
+++ src/async-push.ts
@@ -11,3 +11,4 @@ export const create: <T, Te = never>(
 
 export * from "./stream/async-push";
 export * from "./stream/convert/async-push-to-sync-push";
+export * from "./stream/convert/async-push-to-async-pull"
