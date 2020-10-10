1b0fb9d9e899d3ab62045efee3f09ac35e62d52b
i-drink-yakult <xiaodong@basefex.com>
2020-10-10 11:49:52 +0800

    add pull test

--- test/pull.test.ts
+++ test/pull.test.ts
@@ -1,23 +1,46 @@
-import "./jest"
-import { forEach } from "../pull";
+import "./jest";
+import { createFrom, forEach } from "../pull";
 
 describe("pull", () => {
-  const data = Array.from("collect query");
+  const data = "collect query";
 
-  // todo; add by method
-  const stream_source = data[Symbol.iterator].bind(data);
+  describe("createFrom", () => {
+    test("same collection", () => {
+      const source = createFrom(data);
+
+      let i = 0;
+      for (const x of source()) {
+        expect(x).toBe(data.charAt(i));
+        i++;
+      }
+    });
+
+    test("immutable", () => {
+      const source = createFrom(data);
+
+      for (let count = 2; 0 < count; count--) {
+        let i = 0;
+        for (const x of source()) {
+          expect(x).toBe(data.charAt(i));
+          i++;
+        }
+      }
+    });
+  });
 
   describe("forEach", () => {
+    const source = createFrom(data);
+
     test("run times", () => {
       const mockFn = jest.fn();
-      forEach(stream_source, mockFn);
+      forEach(source, mockFn);
 
       expect(mockFn.mock.calls.length).toBe(data.length);
     });
-    
+
     test("run order", () => {
       const mockFn = jest.fn();
-      forEach(stream_source, mockFn);
+      forEach(source, mockFn);
 
       let i = 0;
       for (const x of data) {
