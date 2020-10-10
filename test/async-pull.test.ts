098cf8452da0e3a6360ed0129b372fa179db38f7
i-drink-yakult <xiaodong@basefex.com>
2020-10-10 15:02:01 +0800

    complement test

--- test/async-pull.test.ts
+++ test/async-pull.test.ts
@@ -1,26 +1,26 @@
 import "./jest";
-import { createFrom, forEach } from "../pull";
+import { createFrom, forEach } from "../async-pull";
 
-describe("pull", () => {
+describe("async pull", () => {
   const data = "collect query";
 
   describe("createFrom", () => {
-    test("same collection", () => {
+    test("same collection", async () => {
       const source = createFrom(data);
 
       let i = 0;
-      for (const x of source()) {
+      for await (const x of source()) {
         expect(x).toBe(data.charAt(i));
         i++;
       }
     });
 
-    test("immutable", () => {
+    test("immutable", async () => {
       const source = createFrom(data);
 
       for (let count = 2; 0 < count; count--) {
         let i = 0;
-        for (const x of source()) {
+        for await (const x of source()) {
           expect(x).toBe(data.charAt(i));
           i++;
         }
@@ -31,22 +31,35 @@ describe("pull", () => {
   describe("forEach", () => {
     const source = createFrom(data);
 
-    test("run times", () => {
+    test("run times", async () => {
       const mock_fn = jest.fn();
-      forEach(source, mock_fn);
+      await forEach(source, mock_fn);
 
       expect(mock_fn).toHaveBeenCalledTimes(data.length);
     });
 
-    test("run order", () => {
+    test("run order", async () => {
       const mock_fn = jest.fn();
-      forEach(source, mock_fn);
+      await forEach(source, mock_fn);
 
       let i = 0;
       for (const x of data) {
         expect(mock_fn.mock.calls[i]?.[0]).toBe(x);
         i++;
       }
+    });
+
+    test("run async", async () => {
+      const data = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+      const source = createFrom(data);
+
+      const test: number[] = [];
+      await forEach(source, async (x) => {
+        await new Promise((r) => setTimeout(r, x * 10));
+        test.push(x);
+      });
+
+      expect(test).toEqual(data);
     });
   });
 });
