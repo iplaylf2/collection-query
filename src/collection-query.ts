f0bba67db79f7d804db4a6be05f2cc5f936eb470
iplaylf2 <914637118@qq.com>
2020-11-28 13:15:01 +0800

    add transfer

--- src/collection-query.ts
+++ src/collection-query.ts
@@ -8,6 +8,23 @@ export {
 export { IterateItem } from "./stream/pull/type";
 export { EmitType, EmitItem } from "./stream/push/type";
 
-export function pipe<T = any, K = T>(list: any[]): (x: T) => K {
-  return (s) => list.reduce((r, f) => f(r), s);
+type Map<T, K> = (x: T) => K;
+type MapD<T extends Map<K, any>, K> = T extends Map<K, infer U> ? U : never;
+
+type RecurMapD<T extends Map<any, any>[], K> = {
+  0: K;
+  1: ((..._: T) => any) extends (_: infer U, ...__: infer R) => any
+    ? U extends Map<K, any>
+      ? R extends Map<any, any>[]
+        ? RecurMapD<R, MapD<U, K>>
+        : never
+      : never
+    : never;
+}[T extends [any, ...any[]] ? 1 : 0];
+
+export function transfer<T, K extends Map<any, any>[]>(
+  s: T,
+  list: [...K]
+): RecurMapD<[...K], T> {
+  return list.reduce((r, f) => f(r), s) as any;
 }
