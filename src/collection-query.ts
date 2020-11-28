4f007fbec34a158af4b0b7a899966d7c269aeb7c
iplaylf2 <914637118@qq.com>
2020-11-28 13:45:50 +0800

    support recur

--- src/collection-query.ts
+++ src/collection-query.ts
@@ -9,22 +9,22 @@ export { IterateItem } from "./stream/pull/type";
 export { EmitType, EmitItem } from "./stream/push/type";
 
 type Map<T, K> = (x: T) => K;
-type MapD<T extends Map<K, any>, K> = T extends Map<K, infer U> ? U : never;
+type DoMap<T extends Map<K, any>, K> = T extends Map<K, infer U> ? U : never;
 
-type RecurMapD<T extends Map<any, any>[], K> = {
-  0: K;
-  1: ((..._: T) => any) extends (_: infer U, ...__: infer R) => any
-    ? U extends Map<K, any>
-      ? R extends Map<any, any>[]
-        ? RecurMapD<R, MapD<U, K>>
-        : never
+type RecurMap<T extends Map<any, any>[], K> = [...T] extends [
+  infer U,
+  ...infer R
+]
+  ? U extends Map<K, any>
+    ? R extends Map<any, any>[]
+      ? RecurMap<R, DoMap<U, K>>
       : never
-    : never;
-}[T extends [any, ...any[]] ? 1 : 0];
+    : never
+  : K;
 
 export function transfer<T, K extends Map<any, any>[]>(
   s: T,
   list: [...K]
-): RecurMapD<[...K], T> {
+): RecurMap<[...K], T> {
   return list.reduce((r, f) => f(r), s) as any;
 }
