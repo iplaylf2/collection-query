f7c8982e40896aab3863b9effb78a3f93c2292f0
i-play-lf2 <914637118@qq.com>
2021-01-26 13:56:31 +0800

    improve

--- src/stream/convert/async-push-to-async-pull.ts
+++ src/stream/convert/async-push-to-async-pull.ts
@@ -1,24 +1,28 @@
 import { AsyncPushStream, AsyncPullStream } from "../type";
-import { EmitType } from "../push/type";
+import { Cancel, EmitType } from "../push/type";
 import { RaceHandler } from "../common/async/race-handler";
 
 export function pull<T>(s: AsyncPushStream<T>): AsyncPullStream<T> {
   return async function* () {
     const handler = new RaceHandler<T>(1);
 
-    const cancel = s(async (t, x?) => {
-      switch (t) {
-        case EmitType.Next:
-          (await handler.race(x)) || cancel();
-          break;
-        case EmitType.Complete:
-          handler.end();
-          break;
-        case EmitType.Error:
-          handler.crash(x);
-          break;
-      }
-    });
+    let cancel!: Cancel;
+    s(
+      async (t, x?) => {
+        switch (t) {
+          case EmitType.Next:
+            (await handler.race(x)) || cancel();
+            break;
+          case EmitType.Complete:
+            handler.end();
+            break;
+          case EmitType.Error:
+            handler.crash(x);
+            break;
+        }
+      },
+      (c) => (cancel = c)
+    );
 
     yield* handler;
   };
